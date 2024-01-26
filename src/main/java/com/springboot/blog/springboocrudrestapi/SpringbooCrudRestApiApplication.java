10bd207d50b45028e1dfddd19a1faf14a1b00c07
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-01-26 07:35:37 -0600

    first commit

---
+++ src/main/java/com/springboot/blog/springboocrudrestapi/SpringbooCrudRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.springboocrudrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooCrudRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooCrudRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/blog/springboocrudrestapi/SpringbooCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.blog.springboocrudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooCrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

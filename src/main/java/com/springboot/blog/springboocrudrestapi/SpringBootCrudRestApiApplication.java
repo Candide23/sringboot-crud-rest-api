9bcefcdd667ae3560f72684550ffe0592031c100
Candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2024-03-07 16:20:06 -0600

    added definition of API information

--- src/main/java/com/springboot/blog/springboocrudrestapi/SpringBootCrudRestApiApplication.java
+++ src/main/java/com/springboot/blog/springboocrudrestapi/SpringBootCrudRestApiApplication.java
@@ -1,11 +1,30 @@
 package com.springboot.blog.springboocrudrestapi;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Blog App REST APIs",
+				description = "Spring Boot Blog App REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "mbk",
+						email = "mboungoukimpolo@gmail.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot Blog App Documentation",
+				url = "https://github.com/Candide23"
+		)
+)
 @SpringBootApplication
 public class SpringBootCrudRestApiApplication {
 
